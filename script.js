41379a70ad64ffa691d297617dbbce0d5ac16930
dailyaitools <ghanshyams481@gmail.com>
2026-01-18 19:28:34 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+const searchInput = document.getElementById("search");
+const cards = document.querySelectorAll(".tool-card");
+
+searchInput.addEventListener("keyup", () => {
+  const value = searchInput.value.toLowerCase();
+
+  cards.forEach(card => {
+    const text = card.innerText.toLowerCase();
+    card.style.display = text.includes(value) ? "block" : "none";
+  });
+});
